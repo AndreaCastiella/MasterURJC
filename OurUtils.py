2b12a94a8d61e140efaf4b2ba689cf4e8aa32103
AndreaCastiella <47426965+AndreaCastiella@users.noreply.github.com>
2021-10-10 19:41:15 +0200

    Add files via upload

--- OurUtils.py
+++ OurUtils.py
@@ -3,12 +3,12 @@
 import pandas as pd
 import numpy as np
 
-# Función para añadir ruido para mejorar la visualización e interpretación de los datos.
+# Function to add noise to improve the visualization and interpretation of the data.
 def jitter(data, sigma=0.3):
     random_sign = (-1.)**np.random.randint(1, high=3, size=data.shape)
-    return data + np.random.normal(0, sigma, size=data.shape)*random_sign
+    return data + np.random.normal(0, sigma, size=data.shape) * random_sign
 
-# Función para separar los datos de entrenamiento de los datos de validación. Random.
+# Function to separate training data from validation data. Random.
 def single_stratified_split(X, Y, test_size=.2, random_state=1234):
     from sklearn.model_selection import StratifiedShuffleSplit
     splitter = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=random_state)
@@ -20,19 +20,20 @@ def single_stratified_split(X, Y, test_size=.2, random_state=1234):
         Y_test  = Y.loc[test_ix].reset_index(drop=True)
     return X_train, Y_train, X_test, Y_test
 
-# Función para concatenar características con las etiquetas.
+# Function to concatenate features with tags.
 def join_features_labels(X0, X1):
     Y0 = pd.DataFrame(np.zeros(X0.shape[0]),columns=['label'])
-    XY0 = pd.concat([X0,Y0],axis=1)
+    XY0 = pd.concat([X0,Y0],axis = 1)
     Y1 = pd.DataFrame(np.ones(X1.shape[0]),columns=['label'])
-    XY1 = pd.concat([X1,Y1],axis=1)
-    return pd.concat([XY0,XY1],axis=0,ignore_index=True)
+    XY1 = pd.concat([X1,Y1],axis = 1)
+    return pd.concat([XY0,XY1],axis = 0,ignore_index=True)
 
-def ajuste_numero(img):
-    # width
+# Function to adjust the image to the number.
+def number_adjust(img):
+    # Width
     col = img.sum(axis=0)
     indc = np.argwhere(col > 0)
-    # height
+    # Height
     row = img.sum(axis=1)
     indr = np.argwhere(row > 0)
     img_rec = img[int(indr[0]):int(indr[-1]), int(indc[0]):int(indc[-1])]
